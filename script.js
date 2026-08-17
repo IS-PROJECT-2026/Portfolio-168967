804e5f6dc260ffee4418f21c30e1abc74e6dd752
mark <marktrulypushingp@gmail.com>
2026-08-17 20:28:18 +0300

    feat: interactivity in the portfolio through filtering

---
+++ script.js
@@ -0,0 +1,30 @@
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".project-card");
+
+filterButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const filter = button.getAttribute("data-filter");
+
+        filterButtons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        projectCards.forEach(card => {
+
+            const category = card.getAttribute("data-category");
+
+            if (filter === "all" || category === filter) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
